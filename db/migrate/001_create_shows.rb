565111373b7495a2761d7fa243d8a835a0c5c27f
Nathan  Visak <nathan.visak@gmail.com>
2018-08-09 21:18:05 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,9 @@
 class CreateShows < ActiveRecord::migration
-  def change 
+  def change
     create_table :shows do |t|
       t.string :name
       t.string :network
       t.string :day
       t.integer :rating
-  end 
-end 
+  end
+end
